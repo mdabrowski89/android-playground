e183acd1266706bc64d660db2bdc327debe158e6
mdabrowski89 <mobite.app@gmail.com>
2020-04-02 16:07:50 +0200

    WIP

--- features/transitions/src/main/java/pl/mobite/playground/transitions/BottomFragment.kt
+++ features/transitions/src/main/java/pl/mobite/playground/transitions/BottomFragment.kt
@@ -2,9 +2,12 @@ package pl.mobite.playground.transitions
 
 import android.os.Bundle
 import android.transition.Slide
+import android.transition.Transition
 import android.transition.TransitionInflater
+import android.transition.TransitionSet
 import android.view.Gravity
 import android.view.View
+import androidx.core.view.ViewCompat
 import kotlinx.android.synthetic.main.fragment_right.*
 import pl.mobite.playground.common.BaseFragment
 
@@ -12,7 +15,7 @@ class BottomFragment : BaseFragment(R.layout.fragment_bottom) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enterTransition = Slide(Gravity.BOTTOM)
+        enterTransition = TransitionSet().addTransition(Slide(Gravity.BOTTOM)).addTransition(ZTransition())
         sharedElementEnterTransition =
             TransitionInflater.from(context).inflateTransition(android.R.transition.move)
     }
--- features/transitions/src/main/java/pl/mobite/playground/transitions/CenterFragment.kt
+++ features/transitions/src/main/java/pl/mobite/playground/transitions/CenterFragment.kt
@@ -1,19 +1,20 @@
 package pl.mobite.playground.transitions
 
+import android.animation.Animator
 import android.os.Bundle
+import android.transition.Explode
 import android.transition.Fade
 import android.transition.Slide
+import android.transition.TransitionValues
+import android.transition.Visibility
 import android.view.Gravity
 import android.view.View
+import android.view.ViewGroup
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import kotlinx.android.synthetic.main.fragment_center.*
 import pl.mobite.playground.common.BaseFragment
 
 class CenterFragment : BaseFragment(R.layout.fragment_center) {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -25,7 +26,8 @@ class CenterFragment : BaseFragment(R.layout.fragment_center) {
                 .navigate(extras)
         }
         moveDown.setOnClickListener {
-            exitTransition = Fade(Fade.MODE_OUT)
+            //exitTransition = Fade(Fade.OUT)//.apply { mode = Fade.MODE_OUT }
+            exitTransition = StayTransition()
             val extras = FragmentNavigatorExtras(sharedView to "sharedElement")
             CenterFragmentDirections
                 .actionCenterToBottom()
