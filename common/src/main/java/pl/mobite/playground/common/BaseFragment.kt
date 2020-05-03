09f31d00571d913f9f53217d0088a3ef05e13bdb
mdabrowski89 <mobite.app@gmail.com>
2020-05-03 20:38:27 +0200

    Remove unused imports

--- common/src/main/java/pl/mobite/playground/common/BaseFragment.kt
+++ common/src/main/java/pl/mobite/playground/common/BaseFragment.kt
@@ -2,8 +2,6 @@ package pl.mobite.playground.common
 
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavDirections
-import androidx.navigation.fragment.FragmentNavigator
 import androidx.navigation.fragment.findNavController
 
 abstract class BaseFragment(@LayoutRes layoutId: Int) : Fragment(layoutId) {
